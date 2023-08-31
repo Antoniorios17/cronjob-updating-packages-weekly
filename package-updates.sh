2c9ff7f5fec1a38693a39fbf592946b4f9090b71
Antonio Rios <Antoniorioscarballo17@gmail.com>
2023-08-31 19:18:30 -0400

    updated script

--- package-updates.sh
+++ package-updates.sh
@@ -3,8 +3,11 @@
 # This command will update the packages on the system
 sudo apt update
 
+# This command will list all the packages that can be upgraded
+# The list will be added to a file for a storage
 apt list --upgradable > test.txt
 
 cat test.txt | tail -n +2 > test.txt
 
 cat test.txt | wc -l
+
